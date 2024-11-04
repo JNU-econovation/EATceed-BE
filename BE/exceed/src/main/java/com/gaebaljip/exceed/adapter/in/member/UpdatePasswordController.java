aa1b02e9a2e93a6b376603a54189965783702df7
hwangdaesun <hds650588@gmail.com>
2024-11-04 22:15:33 +0900

    feat/#553 : @SecurityRequirement 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
@@ -20,12 +20,14 @@ import com.gaebaljip.exceed.common.swagger.ApiErrorExceptionsExample;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@SecurityRequirement(name = "access-token")
 @Tag(name = SwaggerTag.ACCOUNT_MANAGEMENT)
 public class UpdatePasswordController {
 
