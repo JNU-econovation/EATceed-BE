4395754add7de45bd987678376cfaf9ebd185b27
hwangdaesun <hds650588@gmail.com>
2024-11-09 19:15:32 +0900

    refactor/#554 : 괄호 위치 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/domain/meal/GStrategy.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/domain/meal/GStrategy.java
@@ -3,6 +3,6 @@ package com.gaebaljip.exceed.application.domain.meal;
 public class GStrategy implements MeasureStrategy {
     @Override
     public double measure(double value, Unit unit, double servingSize) {
-        return value * (unit.getG() / servingSize);
+        return (value / servingSize) * unit.getG();
     }
 }
