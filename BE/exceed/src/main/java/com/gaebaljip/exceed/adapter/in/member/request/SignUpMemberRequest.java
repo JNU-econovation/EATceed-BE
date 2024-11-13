d11116115c65dd4579843872459816dc3c79b5f3
hwangdaesun <hds650588@gmail.com>
2024-11-13 17:15:00 +0900

    feat/#558 : email 필드에 @NotEmpty 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/request/SignUpMemberRequest.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/request/SignUpMemberRequest.java
@@ -1,6 +1,7 @@
 package com.gaebaljip.exceed.adapter.in.member.request;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 import com.gaebaljip.exceed.common.ValidationMessage;
 import com.gaebaljip.exceed.common.annotation.Password;
@@ -8,7 +9,9 @@ import com.gaebaljip.exceed.common.annotation.Password;
 import lombok.Builder;
 
 public record SignUpMemberRequest(
-        @Email(message = ValidationMessage.INVALID_EMAIL) String email,
+        @Email(message = ValidationMessage.INVALID_EMAIL)
+                @NotEmpty(message = "이메일을 " + ValidationMessage.NOT_EMPTY)
+                String email,
         @Password(message = ValidationMessage.INVALID_PASSWORD) String password) {
     @Builder
     public SignUpMemberRequest {}
