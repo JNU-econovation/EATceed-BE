4f0c029edf4c56e6df2ea2d6737d5314fa063d81
hwangdaesun <hds650588@gmail.com>
2024-11-13 17:13:24 +0900

    feat/#558 : email 필드에 @NotEmtpy 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/request/FindPasswordRequest.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/request/FindPasswordRequest.java
@@ -2,6 +2,7 @@ package com.gaebaljip.exceed.adapter.in.member.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import com.gaebaljip.exceed.common.ValidationMessage;
 import com.gaebaljip.exceed.common.annotation.Password;
@@ -9,9 +10,11 @@ import com.gaebaljip.exceed.common.annotation.Password;
 import lombok.Builder;
 
 public record FindPasswordRequest(
-        @Email(message = ValidationMessage.INVALID_EMAIL) String email,
+        @Email(message = ValidationMessage.INVALID_EMAIL)
+                @NotEmpty(message = "이메일을 " + ValidationMessage.NOT_EMPTY)
+                String email,
         @Password(message = ValidationMessage.INVALID_PASSWORD) String newPassword,
-        @NotBlank(message = "인증 코드를  " + ValidationMessage.NOT_BLANK) String code) {
+        @NotBlank(message = "인증 코드를 " + ValidationMessage.NOT_BLANK) String code) {
 
     @Builder
     public FindPasswordRequest {}
