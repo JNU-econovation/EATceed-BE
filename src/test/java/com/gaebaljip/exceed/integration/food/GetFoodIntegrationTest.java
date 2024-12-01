6a27fd25a476c1033373b450e8843058e6ae4218
Jin Hyuk <ekrrdj07@gmail.com>
2024-12-01 14:55:47 +0900

    fix: when_getFoodsAuto_expected_success 테스트 수정

--- src/test/java/com/gaebaljip/exceed/integration/food/GetFoodIntegrationTest.java
+++ src/test/java/com/gaebaljip/exceed/integration/food/GetFoodIntegrationTest.java
@@ -61,7 +61,7 @@ public class GetFoodIntegrationTest extends IntegrationTest {
         resultActions
                 .andExpectAll(
                         status().isOk(),
-                        jsonPath("$.response.foodJson[0]").value("연어구이:6968;"),
+                        jsonPath("$.response.foodJson[0]").value("연어구이:908;"),
                         jsonPath("$.response.foodJson[1]").value("연어롤:8;"))
                 .andDo(
                         document(
