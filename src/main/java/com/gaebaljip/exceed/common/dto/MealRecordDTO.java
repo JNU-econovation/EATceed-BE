6e106fe20a7425478ce74eae74553668a07bdf55
hwangdaesun <hds650588@gmail.com>
2024-12-05 15:59:11 +0900

    refactor/#590 : 정적 팩터리 메서드 of 이용해 DTO안에서 DTO 생성하도록 수정

--- src/main/java/com/gaebaljip/exceed/common/dto/MealRecordDTO.java
+++ src/main/java/com/gaebaljip/exceed/common/dto/MealRecordDTO.java
@@ -2,9 +2,11 @@ package com.gaebaljip.exceed.common.dto;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.gaebaljip.exceed.application.domain.meal.MealEntity;
 import com.gaebaljip.exceed.application.domain.meal.MealType;
 import com.gaebaljip.exceed.common.CustomDoubleSerializer;
 
@@ -22,6 +24,26 @@ public record MealRecordDTO(
         @JsonSerialize(using = CustomDoubleSerializer.class) Double totalCalories,
         String imageUri,
         List<FoodDTO> foodDTOS) {
+    public static MealRecordDTO of(MealEntity mealEntity, String imageUri) {
+        return MealRecordDTO.builder()
+                .mealType(mealEntity.getMealType())
+                .time(mealEntity.getCreatedDate().toLocalTime())
+                .imageUri(imageUri)
+                .totalCalories(
+                        mealEntity.getMealFoodEntities().stream()
+                                .mapToDouble(mealFood -> mealFood.getAdjustedCalorie())
+                                .sum())
+                .foodDTOS(
+                        mealEntity.getMealFoodEntities().stream()
+                                .map(
+                                        mealFood ->
+                                                FoodDTO.builder()
+                                                        .id(mealFood.getFoodEntity().getId())
+                                                        .name(mealFood.getFoodEntity().getName())
+                                                        .build())
+                                .collect(Collectors.toList()))
+                .build();
+    }
 
     @Builder
     public MealRecordDTO {}
