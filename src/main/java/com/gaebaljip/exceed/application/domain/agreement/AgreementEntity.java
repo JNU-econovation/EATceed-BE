7116feefa0ef704e4a8c2932a1faef2b012f3532
hwangdaesun <hds650588@gmail.com>
2024-12-09 15:40:22 +0900

    feat/#575 : 개인정보처리방침 및 약관 등의 수락 여부를 저장하는 AgreementEntity 생성

---
+++ src/main/java/com/gaebaljip/exceed/application/domain/agreement/AgreementEntity.java
@@ -0,0 +1,51 @@
+package com.gaebaljip.exceed.application.domain.agreement;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.gaebaljip.exceed.common.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = AgreementEntity.ENTITY_PREFIX + "_TB")
+public class AgreementEntity extends BaseEntity {
+    public static final String ENTITY_PREFIX = "AGREEMENT";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ENTITY_PREFIX + "_PK", nullable = false)
+    private Long id;
+
+    @Column(
+            name = ENTITY_PREFIX + "_IS_PRIVACY_POLICY_AGREE",
+            columnDefinition = "tinyint",
+            nullable = false)
+    private Boolean isPrivacyPolicyAgree = true;
+
+    @Column(
+            name = ENTITY_PREFIX + "_IS_TERMS_SERVICE_AGREE",
+            columnDefinition = "tinyint",
+            nullable = false)
+    private Boolean isTermsServiceAgree = true;
+
+    @Column(name = ENTITY_PREFIX + "_IS_OVER_AGE", columnDefinition = "tinyint", nullable = false)
+    private Boolean isOverAge = true;
+
+    @Builder
+    private AgreementEntity(
+            Boolean isPrivacyPolicyAgree, Boolean isTermsServiceAgree, Boolean isOverAge) {
+        this.isPrivacyPolicyAgree = isPrivacyPolicyAgree;
+        this.isTermsServiceAgree = isTermsServiceAgree;
+        this.isOverAge = isOverAge;
+    }
+}
