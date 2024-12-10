664dcf6d9e9bf884ab79959bafa961a22ccc55fc
hwangdaesun <hds650588@gmail.com>
2024-12-10 19:23:41 +0900

    feat/#597 : 개인 정보 처리방침 동의하는 유스케이스 구현
    개인정보 처리 방침을 동의하지 않을 경우 예외 발생

---
+++ src/main/java/com/gaebaljip/exceed/application/service/agreement/AgreePrivacyPolicyService.java
@@ -0,0 +1,31 @@
+package com.gaebaljip.exceed.application.service.agreement;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gaebaljip.exceed.application.domain.member.MemberEntity;
+import com.gaebaljip.exceed.application.port.in.agreement.AgreePrivacyPolicyUsecase;
+import com.gaebaljip.exceed.application.port.out.member.MemberPort;
+import com.gaebaljip.exceed.common.exception.agreement.InvalidPrivacyPolicyStatusException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AgreePrivacyPolicyService implements AgreePrivacyPolicyUsecase {
+    private final MemberPort memberPort;
+
+    @Override
+    @Transactional
+    public void execute(Long memberId, boolean isPrivacyPolicyAgree) {
+        MemberEntity memberEntity = memberPort.query(memberId);
+        validatePrivacyPolicyAgree(isPrivacyPolicyAgree);
+        memberEntity.getAgreementEntity().agreePrivacyPolicy(isPrivacyPolicyAgree);
+    }
+
+    private void validatePrivacyPolicyAgree(boolean isPrivacyPolicyAgree) {
+        if (!isPrivacyPolicyAgree) {
+            throw InvalidPrivacyPolicyStatusException.EXCEPTION;
+        }
+    }
+}
